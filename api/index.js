eb28366f09b8b7d337ca3359b3ff7905ad495cdc
LA18 <luisangelhernandez02605@gmail.com>
2025-03-25 03:55:35 -0600

    Configurar DB y condicionar conexión

--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-// api/index.js
 import serverless from "serverless-http";
 import app from "../src/app.js";
 import mongoose from "mongoose";
@@ -7,6 +6,7 @@ let isConnected = false;
 async function connectDB() {
   if (!isConnected && process.env.MONGODB_URI) {
     console.log("Intentando conectar a la DB...");
+    console.log("MONGODB_URI =>", process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
     isConnected = true;
     console.log("DB conectada");
@@ -16,9 +16,12 @@ async function connectDB() {
 const server = serverless(app);
 
 export default async function handler(req, res) {
-  // Si la ruta es /api/health, saltar la conexión a la DB
-  if (req.url !== '/api/health') {
-    await connectDB();
+  // Si es favicon o health, no te conectes a la DB:
+  if (req.url === '/favicon.ico' || req.url === '/api/health') {
+    return server(req, res);
   }
+  // Para el resto, sí conectas:
+  await connectDB();
   return server(req, res);
 }
+
